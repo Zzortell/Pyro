39cedaa390a4e0a46ad6fa2e94923798603b61eb
Rémi Blaise <remi.blaise@gmx.fr>
2015-07-01 21:49:54 +0200

    [Front/Js:Form/Extract] Get video id from an url

--- Resources/public/js/extract_type.js
+++ Resources/public/js/extract_type.js
@@ -1,6 +1,9 @@
 jQuery(function($){
-	var videoSelect 			= $('#zz_pyrobundle_extract_video'),
-		externalVideoIdInput 	= $('#zz_pyrobundle_extract_externalVideo_id');
+	var form 					= $('[name="zz_pyrobundle_extract"]'),
+		videoSelect 			= $('#zz_pyrobundle_extract_video'),
+		externalVideoIdInput 	= $('#zz_pyrobundle_extract_externalVideo_id'),
+		externalVideoIdLabel 	= $('[for="zz_pyrobundle_extract_externalVideo_id"]')
+	;
 	
 	videoSelect.attr('required', true);
 	
@@ -25,4 +28,24 @@ jQuery(function($){
 	
 	videoSelect.trigger('change');
 	externalVideoIdInput.trigger('input');
+	
+	form.on('submit', function ( e ) {
+		if ( externalVideoIdInput.val() ) {
+			url = urlObject({ 'url': externalVideoIdInput.val() });
+			
+			if (
+				url.hostname.indexOf('youtube.com') === -1
+				|| url.pathname !== '/watch'
+				|| typeof url.parameters.v === 'undefined'
+			) {
+				e.preventDefault();
+				
+				alert('Can\'t get the Youtube video\'s id. Please put a valid video url.');
+			}
+			
+			externalVideoIdInput.val( url.parameters.v );
+		}
+	});
+	
+	externalVideoIdLabel.text('Url');
 });
